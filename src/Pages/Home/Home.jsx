31a3c60691312e55235248ad384526a18208bda9
TogrulEminov <togrul40@mail.ru>
2023-09-20 00:20:34 +0400

    togrul/fixex-bug

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Trending from '../../Components/Trending/Trending';
+import HomeSlider from '../../Components/HomeSlider/HomeSlider';
+import Deals from '../../Components/Deals/Deals';
 
 const Home = () => {
   return (
     <main className="main-section">
+      <HomeSlider />
       <Trending />
+      <Deals />
     </main>
   );
 };
