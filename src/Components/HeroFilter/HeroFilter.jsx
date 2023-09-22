1f44d06bb92c093e8d83ae9cc3a3c337a29f2ecd
TogrulEminov <togrul40@mail.ru>
2023-09-22 23:18:30 +0400

    togrul/somde dropdowns is deleting

--- src/Components/HeroFilter/HeroFilter.jsx
+++ src/Components/HeroFilter/HeroFilter.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './HeroFilter.scss';
+import bgImage from '../../Media/bg.jpeg';
 const HeroFilter = () => {
-  return <div></div>;
+  return (
+    <section
+      className="hero-filter"
+      style={{ background: `url(${bgImage})` }}></section>
+  );
 };
 
 export default HeroFilter;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Trending from '../../Components/Trending/Trending';
 import Blogs from '../../Components/Deals/Blogs';
+import HeroFilter from '../../Components/HeroFilter/HeroFilter';
 
 const Home = () => {
   return (
     <main className="main-section">
+      <HeroFilter />
       <Trending />
       <Blogs />
     </main>
