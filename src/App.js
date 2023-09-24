e40824b012a123f01ddf4c96c4a1f69eaa3edee1
TogrulEminov <togrul40@mail.ru>
2023-09-25 02:47:07 +0400

    togrul/create login and header and footer  change

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import BackToTop from './Components/BackToTop/BackToTop';
 import { ROUTES } from './Routes/Routes';
+import { ToastContainer } from 'react-toastify';
 function App() {
   const router = createBrowserRouter(ROUTES);
   return (
     <>
       <RouterProvider router={router}></RouterProvider>
       <BackToTop />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <ToastContainer />
     </>
   );
 }
--- src/Assets/Logo/Logo.jsx
+++ src/Assets/Logo/Logo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../Media/logo-white.svg';
+import logoScroll from '../../Media/primary-logo.jpeg';
 import './Logo.scss';
 const Logo = () => {
   return (
     <Link to="/" className="logo">
-      <img src={logo} alt="logo for travel" />
+      <img src={logo} alt="logo for travel vebsite" className="logo-noscroll" />
+      <img src={logoScroll} alt="logo for travels vebsite" className='logo-scroll'/>
       <span>Walknjoy</span>
     </Link>
   );
