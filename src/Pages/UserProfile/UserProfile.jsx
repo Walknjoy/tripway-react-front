0d1fbaeb5c30339cfec99fe74102c2c6c905877c
gt-nm-1711 <nijat.mansimov_contractor@gaintheory.com>
2023-09-27 21:41:37 +0400

    fixing

--- src/Pages/UserProfile/UserProfile.jsx
+++ src/Pages/UserProfile/UserProfile.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import './UserProfile.scss';
+import useFetch from '../../hooks/useFetch';
 
-const UserProfile = () => {
+function UserProfile() {
+  const { data, loading, error, fetchData } = useFetch("/users/user/profile");
   return (
     <div>
-      
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <>
+          <img src={data.img} alt="Profile" />
+          <h1>{data.username}</h1>
+          <h1>{data.email}</h1>
+          <h1>{data.country}</h1>
+          <h1>{data.city}</h1>
+          <h1>{data.phone}</h1>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default UserProfile
+export default UserProfile;
