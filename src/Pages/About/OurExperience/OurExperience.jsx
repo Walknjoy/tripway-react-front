c0bba83821518d7d88c3e05134751d8d8947b4b6
TogrulEminov <togrul40@mail.ru>
2023-10-12 12:48:24 +0400

    responsive designed is changed

--- src/Pages/About/OurExperience/OurExperience.jsx
+++ src/Pages/About/OurExperience/OurExperience.jsx
@@ -1,55 +1,68 @@
-import React from 'react'
-import Teamwork from '../../../Media/about-img.png'
-import './OurExperience.scss'
+import React from 'react';
+import Teamwork from '../../../Media/about-img.png';
+import './OurExperience.scss';
+import CountUp from 'react-countup';
 const OurExperience = () => {
-    return (
-        <section id='our_experience'>
-            <div className="experience">
-                <div className="img-side">
-                    <div className="experience-img">
-                        <img src={Teamwork} alt="teamwork" />
-                        <div className="col-12 col-lg-6 col-md-12">
-                            <div className="left-side">
-                                <h3>5 Year Of Working Experience</h3>
-                                <p>We Crafted an aweso design library that is robust and intuitive to use.</p>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <section id="about_section">
+      <div className="about-area">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-xl-6 col-lg-6">
+              <div className="about-img-wrapper">
+                <img src={Teamwork} alt="team work" width="100%" />
+                <div className="about-experience">
+                  <h3>Lorem ipsum dolor sit amet.</h3>
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Est, adipisci.
+                  </p>
                 </div>
-                <div className="center">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-12 col-xl-10 col-md-12">
-                                <div className="experience-content">
-                                    <div className="content-title">
-                                        <span>About us</span>
-                                        <h2>Built-With Boostrap 5, a New Experiance</h2>
-                                    </div>
-                                    <div className="content-description">
-                                        <div className="description-up">
-                                            <p>We Crafted an awesome design library that is robust and intuitive to use. No matter you're building a business
-                                                presentation websit or a complex web application our design</p>
-                                        </div>
-                                        <div className="description-down">
-                                            <div className="counter">
-                                                <span>30k</span>
-                                                <h4>Happy Client</h4>
-                                                <p>We Crafted an awesome design library that is robust and</p>
-                                            </div> <div className="counter">
-                                                <span>30k</span>
-                                                <h4>Happy Client</h4>
-                                                <p>We Crafted an awesome design library that is robust and</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
-        </section>
-    )
-}
+            <div className="col-12 col-xl-6 col-lg-6">
+              <div className="about-content-wrapper">
+                <div className="section-title">
+                  <span>About Us</span>
+                  <h2>Lorem ipsum dolor sit amet.</h2>
+                </div>
+                <div className="about-content">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quidem, sunt voluptate inventore dignissimos esse ut
+                    quisquam vitae, dolorem a sed adipisci repellat veritatis,
+                    nobis laborum iure id? Fugit, quis ipsa.
+                  </p>
+                  <div className="counter-up">
+                    <div className="counter">
+                      <div className="countup">
+                        <CountUp end={30} />k
+                      </div>
+                      <h4>Lorem, ipsum.</h4>
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Mollitia, necessitatibus.
+                      </p>
+                    </div>
+                    <div className="counter">
+                      <div className="countup">
+                        <CountUp end={30} />k
+                      </div>
+                      <h4>Lorem, ipsum.</h4>
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Mollitia, necessitatibus.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default OurExperience
+export default OurExperience;
