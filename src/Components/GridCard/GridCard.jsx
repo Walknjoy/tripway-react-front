27c9be7d551d8415e4798de1fc1eb2b3152beb41
MahammedJafarzade <7a4m9bb@code.edu.az>
2023-10-25 02:47:12 +0400

    card

--- src/Components/GridCard/GridCard.jsx
+++ src/Components/GridCard/GridCard.jsx
@@ -4,78 +4,95 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Raiting from '../../Assets/Raiting/Raiting.jsx';
 import { Link } from 'react-router-dom';
 import { FaLocationDot } from 'react-icons/fa6';
+import { AiOutlineCheck } from 'react-icons/ai'
+import {AiOutlineRight} from 'react-icons/ai'
 const GridCard = () => {
   return <div >
     <section id="cards">
       <div className="container">
         <div className="row">
-          <div className="col-9 card">
-            <div className="card-wrapper">
-              <div className="card-left">
-                <figure>
-                  <LazyLoadImage
-                    effect="blur"
-                    className="image-transition"
-                    src="https://mytravel.madrasthemes.com/wp-content/uploads/2022/02/img4-2-300x225.jpeg"
-                  />
-                </figure>
-              </div>
-              <div className="card-center">
-                <div className="card-header">
-                  <p>
-                    Limited Time Offer
-                  </p>
-                  <p><Raiting /></p>
+          <div className="col-9">
+            <div className="card">
+              <div className="card-wrapper">
+                <div className="card-left">
+                  <figure>
+                    <LazyLoadImage
+                      effect="blur"
+                      className="image-transition"
+                      src="https://mytravel.madrasthemes.com/wp-content/uploads/2022/02/img4-2-300x225.jpeg"
+                    />
+                  </figure>
                 </div>
-                <div className="card-title">
-                  <Link to="/">
-                  Empire Prestige Causeway Bay
-                  </Link>
+                <div className="card-center">
+                  <div className="hotel-card-header">
+                    <Link to="/">
+                      Empire Prestige Causeway Bay
+                    </Link>
+                    <Raiting />
+                  </div>
+                  <div className="card-location">
+                    <FaLocationDot />
+                    <span>
+                      <Link to="/">
+                        United Kingdom
+                      </Link>
+                    </span>
+                    <span>
+                      <Link to="/">
+                        Show on map
+                      </Link>
+                    </span>
+                  </div>
+                  <div className="card-futures">
+                    <p>Late Escape Deal</p>
+                  </div>
+                  <div className="card-description">
+                    <div className="room--card-name">
+                      <span>Superior Double or Twin Room with City View</span>
+                      <p>1 double bed</p>
+                    </div>
+                    <div className="room--card-futures">
+                      <span>Breakfast included</span>
+                      <p>
+                        <AiOutlineCheck />
+                        <strong>
+                          Free cancellation
+                        </strong>
+                      </p>
+                      <p>
+                        <AiOutlineCheck />
+                        <strong>
+                        No prepayment needed
+                        </strong>
+                        – pay at the property
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <div className="card-location">
-                  <FaLocationDot/>
-                  <Link to="/">
-                     Greater London, United Kingdom 
-                  </Link>
+                <div className="card-right">
+                    <div className="hotel--raiting-degree">
+                        <p>SuperB</p>
+                        <p>9.2</p>
+                    </div>
+                      <div className="hotel-location">
+                        <p><strong>Location 10</strong></p>
+                      </div>
+                      <div className="hotel--price-details">
+                        <p>2 nights, 2 adults</p>
+                        <div className="price">
+                        <span>AZN 140</span>
+                        <p>AZN 254</p>
+                        </div>
+                        <div className="taxes">
+                        <span>+AZN 8 taxes and charges</span>
+                        </div>
+                      </div>
+                      <div className="details-button">
+                        <button>See availability
+                          <AiOutlineRight/>
+                        </button>
+                      </div>
                 </div>
-              </div>
-              <div className="card-right">
-                <button type='submit'>See Avaibality</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-12">
-            <div className="card-wrapper">
-              <div className="card-left">
-                <figure>
-                  <LazyLoadImage
-                    effect="blur"
-                    className="image-transition"
-                    src="https://mytravel.madrasthemes.com/wp-content/uploads/2022/02/img4-2-300x225.jpeg"
-                  />
-                </figure>
-              </div>
-              <div className="card-center">
-                <div className="card-header">
-                  <p>
-                    Limited Time Offer
-                  </p>
-                  <p><Raiting /></p>
-                </div>
-                <div className="card-title">
-                  <Link to="/">
-                  Empire Prestige Causeway Bay
-                  </Link>
-                </div>
-                <div className="card-location">
-                  <FaLocationDot/>
-                  <Link to="/">
-                     Greater London, United Kingdom 
-                  </Link>
-                </div>
-              </div>
-              <div className="card-right">
-                <button type='submit'>See Avaibality</button>
               </div>
             </div>
           </div>
