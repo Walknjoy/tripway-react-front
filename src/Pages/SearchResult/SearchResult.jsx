a7194c77ec42c6ada5abe920b23395355add1450
TogrulEminov <togrul40@mail.ru>
2023-10-29 23:21:16 +0400

    searching result pages and  some filters box

--- src/Pages/SearchResult/SearchResult.jsx
+++ src/Pages/SearchResult/SearchResult.jsx
@@ -1,25 +1,49 @@
-import React, { useContext } from 'react'
-import { mainContext } from '../../utils/ContextApi'
-
+import PageTitle from '../../Components/PageTitle/PageTitle';
+import "./SearchResult.scss"
+import {BsFillGrid3X3GapFill, BsListUl} from "react-icons/bs"
+import SearchFilterArea from './SearchFilterArea/SearchFilterArea';
 const SearchResult = () => {
-    const {filterList}=useContext(mainContext)
   return (
-    <div>
-<div style={{height:"200px"}}></div>      
-    {filterList?.map((hotelContent)=>{
-        return <div key={hotelContent?._id}>
-            {hotelContent.city}
-            {hotelContent?.price}
-            {hotelContent.desc}
-            {hotelContent.rooms?.map((ret,index)=>(
-                <span key={index}>{ret.title}</span>
-            ))}
-
-
+    <>
+      <PageTitle
+        title={'Hotel result'}
+        backgroundImg={
+          'https://cdn2.paraty.es/bg-corporativa/images/6a92de9242aca95=s1600'
+        }
+      />
+      <div className="all_result_page">
+        <div className="container">
+          <div className="result_wrapper">
+            <div className="result_left">
+             <SearchFilterArea/>
+            </div>
+            <div className="result_right">
+              <div className="row">
+                <div className="col-12">
+                  <div className="products-header">
+                    <div className="products_title">
+                      <h3>
+                        Hotel:
+                        <span>7 results found</span>
+                      </h3>
+                    </div>
+                    <div className="products_grid_btn">
+                        <button>
+                            <BsListUl/>
+                        </button>
+                        <button>
+                            <BsFillGrid3X3GapFill/>
+                        </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    })}
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default SearchResult
+export default SearchResult;
